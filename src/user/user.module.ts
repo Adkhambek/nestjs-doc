3c58ccab2799eba8c0d7588b2351bb5de584eeec
Adham <muzaffarov.adham@gmail.com>
2022-04-03 16:31:12 +0500

    middleware

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,22 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { UserMiddleware } from './middleware/user.middleware';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UserMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
